1e5ac5f0fdf3a93eeb52f807387706e1a9719f66
RCJacH <RCJacH@outlook.com>
2023-01-27 15:59:44 +0800

    fix test

--- tests/test_get_sector_pairs.py
+++ tests/test_get_sector_pairs.py
@@ -11,13 +11,14 @@ class TestPos:
             ([0, 0.3, 0.6], [0, 0.5], [0, 0.5, 0.5]),
             ([0, 0.8, 0.9], [0, 0.5], [0, 0.5, 1]),
             ([0, 0.1, 0.2], [0, 0.75], [0, 0, 0.75]),
-            ([0, 0.1, 0.2, 0.9], [0, 0.75], [0, 0, 0.75, 1]),
+            ([0, 0.1, 0.2, 0.9], [0, 0.75], [0, 0, 0.75, 0.75]),
             ([0, 0.25, 0.5, 0.75], [0, 0.5], [0, 0.5, 0.5, 1]),
+            ([0, 0.25, 0.5, 0.75], [0, 0.2, 0.5], [0, 0.2, 0.5, 1]),
         ],
     )
     def test_shrinking(self, more_pos, less_pos, expectation):
         more_array = np.stack([more_pos, (*more_pos[1:], 1)], dtype="float64")
         less_array = np.stack([less_pos, (*less_pos[1:], 1)], dtype="float64")
         expectation = np.array(expectation, dtype="float64")
-        indice = [1 if x == 1 else less_pos.index(x) for x in expectation]
+        indice = [-1 if x == 1 else less_pos.index(x) for x in expectation]
         assert get_sector_pairs(less_array, more_array) == indice
