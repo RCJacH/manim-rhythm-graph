618247202b7cc08e8600d7ee858d02ee8f65d265
RCJacH <RCJacH@outlook.com>
2023-01-27 16:01:11 +0800

    add Radii class

--- src/manim_rhythm_graph/pie_mobject/pie.py
+++ src/manim_rhythm_graph/pie_mobject/pie.py
@@ -4,6 +4,40 @@ import manim as mn
 from .pie_sector import PieSector
 
 
+class Radii(mn.Group):
+    def __init__(
+        self, angles, radius, stroke_color, stroke_width, color=None, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.angles = [*angles[:, 0], angles[-1].sum()]
+        self.radius = radius
+        self.stroke_color = stroke_color
+        self.stroke_width = stroke_width
+        self.add(
+            *(
+                PieSector(
+                    radius=radius,
+                    start_angle=a,
+                    angle=0,
+                    color=color,
+                    stroke_color=stroke_color,
+                    stroke_width=stroke_width,
+                    **kwargs,
+                )
+                for a in self.angles
+            )
+        )
+
+    @mn.override_animation(mn.Uncreate)
+    def _uncreate_override(self, lag_ratio=0, rate_func=lambda _: 1, **kwargs):
+        return mn.AnimationGroup(
+            *(mn.Uncreate(x, remover=True, rate_func=rate_func) for x in self),
+            lag_ratio=lag_ratio,
+            remover=True,
+            **kwargs,
+        )
+
+
 class Pie(mn.VGroup):
     def __init__(self, divisions, radius=1, colors=None, **kwargs):
         stroke_color = kwargs.pop("stroke_color", mn.WHITE)
