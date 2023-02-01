1ef413abe54cb19261f3fd3d856e378639e98358
RCJacH <RCJacH@outlook.com>
2023-02-02 02:50:55 +0800

    combine preview files into one

--- tests/preview_rhythm_element.py
+++ tests/preview_rhythm_element.py
@@ -5,12 +5,26 @@ from manim_rhythm_graph import RhythmElement
 
 class TestRhythmElement(Scene):
     def construct(self):
-        ele = RhythmElement(3, scale=3, style="stick")
+        ele = RhythmElement(
+            weights=5, colors=[RED, BLUE], scale=3, style="pulse"
+        )
         self.play(Create(ele))
         self.wait()
         self.play(ele.pulsate())
         self.wait()
         self.play(ele.as_pie())
+        self.wait()
+        self.play(
+            ele.into(
+                weights=[2, 1, 3, -2, 2, 2],
+                colors=[GOLD, ORANGE, GREEN],
+                scale=2,
+            )
+        )
+        self.wait()
+        self.play(ele.pulsate())
+        self.wait()
+        self.play(ele.as_pulse())
         self.wait()
         self.play(ele.pulsate())
         self.wait()
@@ -18,7 +32,15 @@ class TestRhythmElement(Scene):
         self.wait()
         self.play(ele.pulsate())
         self.wait()
+        self.play(ele.as_pulse())
+        self.wait()
+        self.play(ele.pulsate())
+        self.wait()
         self.play(ele.as_pie())
+        self.wait()
+        self.play(ele.pulsate())
+        self.wait()
+        self.play(ele.as_pulse())
         self.wait()
         self.play(ele.pulsate())
         self.wait()
